4d9d581bea13d6997ea9b1a4e7ce887bac084ba1
rajrajpatel234-lab <rajrajpatel234-lab@users.noreply.github.com>
2026-01-09 14:13:53 +0000

    feat: add dashboard components index for cleaner imports

---
+++ client/src/components/dashboard/index.js
@@ -0,0 +1,10 @@
+// Common Components
+export { default as Sidebar } from './common/Sidebar';
+export { default as TopNav } from './common/TopNav';
+
+// Card Components
+export { default as ProfileCard } from './cards/ProfileCard';
+export { default as MOPTroInfoCard } from './cards/MOPTroInfoCard';
+export { default as UtilizationCard } from './cards/UtilizationCard';
+export { default as PlatformSettingsCard } from './cards/PlatformSettingsCard';
+export { default as ProductsCard } from './cards/ProductsCard';
